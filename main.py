1b4e51425be962098c48b2a5067ed78c6c0c9d7f
mathssantos <matheus0406silva@gmail.com>
2020-08-05 23:53:42 -0300

    Script update to comply with PEP-8

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 
-
 def main_menu():
     print('------------------------------')
     print('1- Inserir uma nova conta')
@@ -16,33 +15,39 @@ def main_menu():
 def clear_terminal():
     return print('\x1b[2J')
 
+
 def save_data(app_n, app_p):
     cursor.execute(f"""
                     INSERT INTO passwords (name, password) VALUES (?, ?)
                    ;""", (app_n, app_p))
     connect.commit()
 
+
 def show_name_apps():
     cursor.execute("""
                     SELECT name FROM passwords
                    ;""")
 
-def recover_password(name):
+
+def recover_password(app_n):
     cursor.execute(f"""
-                    SELECT name, password FROM passwords WHERE name = '{name}'
+                    SELECT name, password FROM passwords WHERE name = '{app_n}'
                   ;""")
 
-def edit_account(app_n, new_pass):
+
+def edit_account(app_n, new_password):
     cursor.execute(f"""
-                   UPDATE passwords SET password = '{new_pass}' WHERE name = '{app_n}'
+                   UPDATE passwords SET password = '{new_password}' WHERE name = '{app_n}'
                    ;""")
+
 
 def delete_account(app_n):
     cursor.execute(f"""
                    DELETE FROM passwords WHERE name = '{app_n}'
                    ;""")
 
-#Connect to database
+
+# Connect to database
 connect = sqlite3.connect('passkey.db')
 cursor = connect.cursor()
 
@@ -53,26 +58,30 @@ while True:
     main_menu()
     a = input('Opção: ')
 
-    if a == '1': # Input of new app and password
+    if a == '1':  # Input of new app and password
         app_name = input('Insira o nome do app: ')
         app_password = input('Insira a senha: ')
         save_data(app_name, app_password)
 
     elif a == '2':
-        show_name_apps()# Show all app's saved
+        show_name_apps()  # Show all app's saved
         print('--------------------------')
         print('*****APPS CADASTRADOS*****')
 
         for name in cursor.fetchall():
             print(name[0])
+
         print('--------------------------')
         input('Aperte enter para continuar...')
-    elif a == '3': # Show all app's saved and edit app password
+
+    elif a == '3':  # Show all app's saved and edit app password
         show_name_apps()
         names = []
+
         for name in cursor.fetchall():
             names.append(name[0])
             print(name[0])
+
         while True:
             account_name = input('Digite o nome da conta que você deseja recuperar a senha: ')
             if account_name in names:
@@ -85,11 +94,14 @@ while True:
     elif a == '4':
         show_name_apps()
         apps = []
+
         for app in cursor.fetchall():
             apps.append(app[0])
             print(app[0])
+
         print('------------------------------')
         print('Só é permitido alterar a senha')
+
         while True:
             account_name = input('Digite o nome da conta que você deseja editar a senha: ')
             if account_name in apps:
@@ -109,7 +121,7 @@ while True:
                 delete_account(account_name)
                 break
 
-    elif a == '0': # End software
+    elif a == '0':  # End software
         break
 connect.close()
 print('Software Finalizado')
